7c043ca1a89f9a7821ed45941ed3ccb25db0297b
LuisHenrique0909 <luis.h.s.d.n@gmail.com>
2025-10-26 20:41:59 -0400

    entendi pra que serve a função quicksort e otimizei as funções de files

--- Utilidades/FILES/Files.c
+++ Utilidades/FILES/Files.c
@@ -4,53 +4,34 @@
 
 #include <string.h>
 
-int ultimo_id(const char *nome_arquivo){
-
+int ultimo_id(const char *nome_arquivo) {
     FILE *f = abrir_csv(nome_arquivo);
     if (!f) return -1;
 
     char linha[256];
     int ultimo = -1;
-
-    fgets(linha, sizeof(linha), f);
-    
-    while( fgets(linha, sizeof(linha), f) ){
-        linha[strcspn(linha, "\n")] = '\0';
-        int id;
-        if (sscanf(linha, "%d,", &id) == 1){
-            ultimo = id;
-        }
-    }
+    fgets(linha, sizeof(linha), f); // pula cabeçalho
+    while (fgets(linha, sizeof(linha), f))
+        sscanf(linha, "%d,", &ultimo);
     fclose(f);
     return ultimo;
 }
 
-void trocar(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void trocar(int *a, int *b) {
+    int t = *a; *a = *b; *b = t;
 }
 
-int particionar(int *lista[], int inicio, int fim){
-    int pivo = *lista[fim];
-    int i = inicio - 1;
-
-    int j;
-    for (j = inicio; j < fim; j++) {
-        if (*lista[j]< pivo){
-            i++;
-            trocar(lista[j], lista[i]);
-        }
-    }
-    i++;
-    trocar(lista[fim], lista[i]);
+int particionar(int *lista[], int inicio, int fim) {
+    int p = *lista[fim], i = inicio - 1;
+    for (int j = inicio; j < fim; j++)
+        if (*lista[j] < p) trocar(lista[++i], lista[j]);
+    trocar(lista[++i], lista[fim]);
     return i;
 }
 
-void quicksort(int *lista[], int inicio, int fim){
-    if(inicio >= fim) return;
-    
-    int indicePivo = particionar(lista, inicio, fim);
-    quicksort(lista, inicio, indicePivo-1);
-    quicksort(lista, indicePivo+1, fim);
+void quicksort(int *lista[], int inicio, int fim) {
+    if (inicio >= fim) return;
+    int p = particionar(lista, inicio, fim);
+    quicksort(lista, inicio, p - 1);
+    quicksort(lista, p + 1, fim);
 }
--- Utilidades/FILES/Files.h
+++ Utilidades/FILES/Files.h
@@ -4,9 +4,7 @@
 #include "User.h"
 #include <stdio.h>
 
-
 int ultimo_id(const char *nome_arquivo);
 void quicksort(int *lista[], int inicio, int fim);
-
 
 #endif
