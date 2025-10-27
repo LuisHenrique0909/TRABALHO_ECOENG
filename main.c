c572d6ffd6f77831bb6d1d1921290eeca5236b5e
LuisHenrique0909 <luis.h.s.d.n@gmail.com>
2025-10-27 18:55:13 -0400

    mudei diretorio

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <locale.h>
 
+#include "Funcoes_cvs/Dados.h"
+#include "Cadastro/Cadastro_Equipes/Cadastro_Equipes.h"
+
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
+    FILE *f = escrever_no_csv("luis.csv", "id,nome");
 
-
+    fprintf(f, "0,higor\n");
     
     return 0;
 }
