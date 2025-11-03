b3321b3747062d123ec9caf4e7237194c98f94b6
LuisHenrique0909 <luis.h.s.d.n@gmail.com>
2025-11-03 19:00:11 -0400

    apenas mudei ordem do codigo, preferencia pessoal

--- Menus/Menu_Equipes/Menu_Equipes.c
+++ Menus/Menu_Equipes/Menu_Equipes.c
@@ -4,39 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Função auxiliar: mostra pontuação da equipe do usuário
-void mostrar_pontuacao_da_equipe(User *usuario_logado) {
-    FILE *f = abrir_csv("resultados_robo.csv");
-    if (!f) {
-        printf("Nenhuma pontuação registrada ainda.\n");
-        return;
-    }
-
-    char linha[512];
-    fgets(linha, sizeof(linha), f); // pula cabeçalho
-
-    char nome_equipe[100];
-    float tempo;
-    int pontos, id;
-    char tipo[50];
-
-    int encontrou = 0;
-
-    while (fgets(linha, sizeof(linha), f)) {
-        sscanf(linha, "%d,%99[^,],%49[^,],%f,%d", &id, nome_equipe, tipo, &tempo, &pontos);
-        if (strstr(nome_equipe, usuario_logado->nome)) {
-            printf("\n--- Pontuação da sua equipe (%s) ---\n", nome_equipe);
-            printf("Desafio: %s\nTempo: %.2f s\nPontos: %d\n", tipo, tempo, pontos);
-            encontrou = 1;
-        }
-    }
-
-    fclose(f);
-
-    if (!encontrou)
-        printf("\nSua equipe ainda não tem pontuação registrada.\n");
-}
-
 // Menu principal da equipe
 void menu_equipe(User *usuario_logado) {
     if (!usuario_logado) {
@@ -72,4 +39,37 @@ void menu_equipe(User *usuario_logado) {
                 printf("Opção inválida.\n");
         }
     } while (opc != 0);
+}
+
+// Função auxiliar: mostra pontuação da equipe do usuário
+void mostrar_pontuacao_da_equipe(User *usuario_logado) {
+    FILE *f = abrir_csv("resultados_robo.csv");
+    if (!f) {
+        printf("Nenhuma pontuação registrada ainda.\n");
+        return;
+    }
+
+    char linha[512];
+    fgets(linha, sizeof(linha), f); // pula cabeçalho
+
+    char nome_equipe[100];
+    float tempo;
+    int pontos, id;
+    char tipo[50];
+
+    int encontrou = 0;
+
+    while (fgets(linha, sizeof(linha), f)) {
+        sscanf(linha, "%d,%99[^,],%49[^,],%f,%d", &id, nome_equipe, tipo, &tempo, &pontos);
+        if (strstr(nome_equipe, usuario_logado->nome)) {
+            printf("\n--- Pontuação da sua equipe (%s) ---\n", nome_equipe);
+            printf("Desafio: %s\nTempo: %.2f s\nPontos: %d\n", tipo, tempo, pontos);
+            encontrou = 1;
+        }
+    }
+
+    fclose(f);
+
+    if (!encontrou)
+        printf("\nSua equipe ainda não tem pontuação registrada.\n");
 }
